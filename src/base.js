cb43f7f335febf276b10ea6a290c00779338c562
Seth Baughman <sbaughman1@me.com>
2017-05-12 15:18:34 -0400

    Add re-base and firebase config

---
+++ src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base'
+
+export default Rebase.createClass({
+  apiKey: "AIzaSyDfnxgpw3QKIQOgstpypgrx0luMjVvxcKU",
+  authDomain: "github-list.firebaseapp.com",
+  databaseURL: "https://github-list.firebaseio.com",
+  projectId: "github-list",
+  storageBucket: "github-list.appspot.com",
+  messagingSenderId: "801921888862"
+})
